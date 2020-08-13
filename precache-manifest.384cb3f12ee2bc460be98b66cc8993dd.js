a010ea91106826979ca77b79bfcf2e6d9aa5a8fa
Sean Courtney <seankc@me.com>
2020-08-12 18:31:38 -0700

    Updates

---
+++ precache-manifest.384cb3f12ee2bc460be98b66cc8993dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39474f09ef341059a1d243ac74d30775",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/RoboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0c6868dfe1ea6ce5bfe7",
+    "url": "/RoboFriends/static/css/main.976aedee.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/RoboFriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c6868dfe1ea6ce5bfe7",
+    "url": "/RoboFriends/static/js/main.5c561f55.chunk.js"
+  },
+  {
+    "revision": "671d1b49487ff131bdad",
+    "url": "/RoboFriends/static/js/runtime-main.16920bcc.js"
+  }
+]);

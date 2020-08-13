52e4435fdd5d6a851c1c6a4eb1988be48c29958f
Sean Courtney <seankc@me.com>
2020-08-12 17:59:18 -0700

    Fixes

--- src/containers/App.js
+++ src/containers/App.js
@@ -24,23 +24,22 @@ class App extends Component {
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const { robots, searchfield }= this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
 
-        if (this.state.robots.length === 0) {
-            return <h1>Loading...</h1>
-        } else {
-                return (
-                <div className='tc'>
-                    <h1>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
-                    <Scroll>
-                        <CardList robots={filteredRobots}/>
-                    </Scroll>
-                </div>
-            );
-        }
+    return !robots.length ?
+        <h1>Loading...</h1> :
+        (
+            <div className='tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll>
+            </div>
+        );
     }
 }
 
